0de2fa1443a4b007085c5fa7fe436dcffce789b2
Samuel Lee <theleesamuel@gmail.com>
2018-03-20 10:33:41 -0700

    build history props

--- frontend/components/history.jsx
+++ frontend/components/history.jsx
@@ -1,18 +1,32 @@
 import React from 'react';
+import _ from 'lodash';
+import {Card, CardHeader, CardText} from 'material-ui/Card';
 
 class History extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      data : 'There is no number history here :('
+    }
   }
 
   componentDidMount() {
+    this.setState({
+      data: this.props.search.search
+    });
   }
 
   render() {
     return (
-      <div>
-        this is history
-      </div>
+      <Card>
+        <CardHeader title='Your Number Search History' />
+        <CardText>
+          {
+            this.state.data
+          }
+        </CardText>
+      </Card>
     );
   }
 }
